8d277ad989c8bf593f53bc17102c7cd50ea415b9
Bilal Javed <imbilalj@gmail.com>
2023-08-11 13:53:53 +0500

    Add mutation to main schema

--- server/graphql/schema/schema.js
+++ server/graphql/schema/schema.js
@@ -1,6 +1,8 @@
 import { GraphQLSchema } from 'graphql';
 import { Query } from './Query.js';
+import { Mutation } from './Mutation.js';
 
 export const schema = new GraphQLSchema({
   query: Query,
+  mutation: Mutation,
 });
